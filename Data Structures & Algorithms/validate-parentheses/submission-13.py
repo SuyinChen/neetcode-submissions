2a3f9b225443c5e3e325384f1f315f6860af3c62
SuyinChen <suyin.m.chen@gmail.com>
2026-08-19 04:02:30 -0700

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        h = {')' : '(', '}' : '{', ']' : '['}
+        # stack = []
+        # for i in range(len(s)):
+        #     if s[i] in h.keys():
+        #         stack.append(s[i])
+        #     else:
+        #         if not stack:
+        #             return False
+        #         if h[stack.pop()] != s[i]:
+        #             return False
+        # if not stack:
+        #     return False
+        # return True
+
+        stack = []
+        for i in range(len(s)):
+            if s[i] not in h:
+                stack.append(s[i])
+            else:
+                if stack and stack[-1] == h[s[i]]:
+                    stack.pop()
+                else:
+                    return False
+        return False if stack else True
+
+
+            
